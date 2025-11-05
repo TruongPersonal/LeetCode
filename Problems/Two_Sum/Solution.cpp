83044bae3c40ee7c38792e54c81fe33de73d36b9
Ngô Quang Trường <ngoquangtruong.isme@gmail.com>
2025-11-05 13:25:42 +0700

    05/11/2025

--- Problems/Two_Sum/Solution.cpp
+++ Problems/Two_Sum/Solution.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution
 {
 public:
@@ -29,24 +25,3 @@ public:
         return result;
     }
 };
-
-int main()
-{
-    vector<int> nums;
-    nums.push_back(2);
-    nums.push_back(7);
-    nums.push_back(11);
-    nums.push_back(15);
-    int target = 9;
-    Solution solution;
-    vector<int> result = solution.twoSum(nums, target);
-    cout << '[';
-    for (int i = 0; i < result.size() - 1; i++)
-    {
-        cout << result[i] << ',';
-    }
-    cout << result[result.size() - 1];
-    cout << ']';
-
-    return 0;
-}
